8a137440335e82825eff310a4d3531a0f1de7f4a
reeyanmaknojiya <47335900+reeyanmaknojiya@users.noreply.github.com>
2021-04-13 15:53:23 -0600

    minor edit

--- cpsc471_project_db.sql
+++ cpsc471_project_db.sql
@@ -273,7 +273,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `userID` int(9) NOT NULL,
   `name` varchar(30) DEFAULT 'noName',
   `DoB` date NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
