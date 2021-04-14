cb4bcab1309bf91d0511ac166057847a57113db9
reeyanmaknojiya <47335900+reeyanmaknojiya@users.noreply.github.com>
2021-04-13 23:38:02 -0600

    Added Dietician Navbar

---
+++ Project/login/Dietician/index.php
@@ -0,0 +1,11 @@
+<?php
+	require('../config/config.php');
+	require('../config/db.php');
+?>
+
+<?php include('inc/dieticianHeader.php'); ?>
+	<div class="container">
+		<h1>Dietician</h1>
+		
+	</div>
+<?php include('inc/footer.php'); ?>
