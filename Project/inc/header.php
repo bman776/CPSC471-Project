f0b5bb1c684f452e8107803a5bad02fddf58cf1d
bman776 <46765260+bman776@users.noreply.github.com>
2021-04-13 22:58:38 -0600

    Delete header.php

--- Project/inc/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>My Fit</title>
-		<link rel="stylesheet" type="text/css" href="https://bootswatch.com/3/cerulean/bootstrap.min.css">
-	</head>
-	
-	<body>
-	<?php include('navbar.php'); ?>
