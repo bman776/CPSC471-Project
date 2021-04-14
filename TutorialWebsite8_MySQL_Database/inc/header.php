97486081b8b7a3578c8371bd2d5a6c4326c2b39d
bman776 <46765260+bman776@users.noreply.github.com>
2021-04-13 23:23:45 -0600

    Delete header.php

--- TutorialWebsite8_MySQL_Database/inc/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>PHP Blog</title>
-		<link rel="stylesheet" type="text/css" href="https://bootswatch.com/3/cerulean/bootstrap.min.css">
-	</head>
-	
-	<body>
-	<?php include('navbar.php'); ?>
