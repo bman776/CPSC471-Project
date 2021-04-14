fa77d16c1b269cdc4e2fc115e93820bfb6452975
bman776 <46765260+bman776@users.noreply.github.com>
2021-04-13 19:49:04 -0600

    Add files via upload
    Deleted Project folder for no reason (woops :/ )

---
+++ Project/index.php
@@ -0,0 +1,11 @@
+<?php
+	require('config/config.php');
+	require('config/db.php');
+?>
+
+<?php include('inc/header.php'); ?>
+	<div clas="container">
+		<h1>Posts</h1>
+	</div>
+<?php include('inc/footer.php'); ?>
+
