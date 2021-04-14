2ab8457527ede73c0444e0d247a64d9966050b90
bman776 <46765260+bman776@users.noreply.github.com>
2021-04-13 19:47:37 -0600

    Delete config.php

--- Project/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	define('ROOT_URL', 'http://localhost/Project/');
-	define('DB_HOST', "localhost:3308");
-	define('DB_USER', "root");
-	define('DB_PASS', "");
-	define('DB_NAME', "phpblog");
-?>
