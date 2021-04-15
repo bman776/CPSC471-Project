27e37f7543bb43c55eaec3c6a323dddf22695545
Brett Gattinger <brett.gattinger@ucalgary.ca>
2021-04-15 12:53:45 -0600

    enhanced login page

--- Project/login/Client/index.php
+++ Project/login/Client/index.php
@@ -1,11 +1,15 @@
 <?php
 	require('../config/config.php');
 	require('../config/db.php');
+
+	//get session
+	session_start();
+
+	$name = $_SESSION['name'];
 ?>
 
 <?php include('inc/clientHeader.php'); ?>
 	<div class="container">
-		<h1>Client</h1>
-
+		<h1>Welcome <?php echo $name; ?></h1>
 	</div>
 <?php include('../inc/footer.php'); ?>
--- Project/login/index.php
+++ Project/login/index.php
@@ -2,38 +2,50 @@
 	require('config/config.php');
 	require('config/db.php');
 
-	if ( isset($_POST['username']) and isset($_POST['password']) ) {
+	if ( isset($_POST['submit']) ) {
 
-		//http://localhost/Project/Login/Instructor/
-		//$loc = INST_ROOT_URL;
-		//header("Location:$loc");
-		$usrnm = $_POST['username'];
-		$psswd = $_POST['password'];
+		if ( isset($_POST['username']) and isset($_POST['password']) ) {
 
-		$query = "SELECT u.username, u.password FROM user AS u WHERE u.username = '$usrnm' AND u.password = '$psswd'";
-		$query2 = "SELECT * FROM user";
+			//query user login credentials
+			$usrnm = $_POST['username'];
+			$psswd = $_POST['password'];
+			$query = "SELECT u.userID, u.name FROM user AS u WHERE u.username = '$usrnm' AND u.password = '$psswd'";
+			$result = mysqli_query($conn, $query);
 
-		$result = mysqli_query($conn, $query);
-		
-		if (!$result) {
-			echo "Query Error: " . mysqli_error($conn);
-		} else {
-			$result_row_count = mysqli_num_rows($result);
-			if ($result_row_count > 0) {
-				echo "You are in the System!";
-				$loc = CLIENT_ROOT_URL;
-				header("Location:$loc");
+			//evaluate result
+			if (!$result) {
+				echo "Query Error: " . mysqli_error($conn);
 			} else {
-				echo "Invalid Username or Password";
+				$result_row_count = mysqli_num_rows($result);
+				if ($result_row_count > 0) {
+					//user credentials valid, begin login
+
+					$result_row = mysqli_fetch_row($result);
+					$id = $result_row[0]; // the id attribute of the user
+					$name = $result_row[1]; // the name attribute of user
+
+					//Start Session
+					session_start();
+					$_SESSION['name'] = $name;
+
+					//Redirect to client profile page (for now)
+					$loc = CLIENT_ROOT_URL;
+					header("Location:$loc");
+
+				} else {
+					echo "Invalid Username or Password";
+				}
 			}
+		} else {
+			echo "Please enter a username and password";
 		}
-	}
+	} 
 ?>
 
 <?php include('inc/loginHeader.php'); ?>
 	<div class="container">
 		<h1>Login</h1>
-		<form class="form-horizontal" method="post" action="index.php">
+		<form class="form-horizontal" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 			<fieldset>
 			
 				<div class="form-group">
@@ -53,7 +65,7 @@
 				<div class="form-group">
 					<div class="col-lg-l0 col-lg-offset-2">
 					<button type="reset" class="btn btn-default">Cancel</button>
-					<button type="submit" class="btn btn-primary">Submit</button>
+					<button type="submit" name="submit" class="btn btn-primary">Submit</button>
 				</div>
 			</fieldset>
 		</form>
